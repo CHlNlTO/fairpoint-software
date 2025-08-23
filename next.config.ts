4f62e4ffc7cc0b997acd0c0da9ef25ba52c816cf
Chinito <clark.wayne023@gmail.com>
2025-08-23 20:51:34 +0800

    Fix eslint boundary not working

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    dirs: ['app', 'components', 'features', 'hooks', 'lib'],
+    ignoreDuringBuilds: false,
+  },
 };
 
 export default nextConfig;
