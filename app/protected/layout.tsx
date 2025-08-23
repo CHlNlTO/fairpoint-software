bbc36374166d3be6907e219ba3cc529f727b2d26
Chinito <clark.wayne023@gmail.com>
2025-08-23 15:31:11 +0800

    Cleanup

--- app/protected/layout.tsx
+++ app/protected/layout.tsx
@@ -1,4 +1,3 @@
-import { DeployButton } from "@/components/ui/deploy-button";
 import { EnvVarWarning } from "@/components/ui/env-var-warning";
 import { AuthButton } from "@/features/auth/auth-button";
 import { ThemeSwitcher } from "@/components/ui/theme-switcher";
@@ -16,10 +15,7 @@ export default function ProtectedLayout({
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
             <div className="flex gap-5 items-center font-semibold">
-              <Link href={"/"}>Fairpoint Software</Link>
-              <div className="flex items-center gap-2">
-                <DeployButton />
-              </div>
+              <Link href={"/"}>Fairpoint</Link>
             </div>
             {!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
           </div>
@@ -28,16 +24,16 @@ export default function ProtectedLayout({
           {children}
         </div>
 
-        <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
+        <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-4">
           <p>
             Powered by{" "}
             <a
-              href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+              href="https://ckdigitals.com"
               target="_blank"
               className="font-bold hover:underline"
               rel="noreferrer"
             >
-              Supabase
+              CKDigitals
             </a>
           </p>
           <ThemeSwitcher />
--- features/home/hero.tsx
+++ features/home/hero.tsx
@@ -1,21 +1,9 @@
-import { NextLogo } from "@/components/ui/next-logo";
-import { SupabaseLogo } from "@/components/ui/supabase-logo";
+
 
 export function Hero() {
   return (
     <div className="flex flex-col gap-16 items-center">
       <div className="flex gap-8 justify-center items-center">
-        <a
-          href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <SupabaseLogo />
-        </a>
-        <span className="border-l rotate-45 h-6" />
-        <a href="https://nextjs.org/" target="_blank" rel="noreferrer">
-          <NextLogo />
-        </a>
       </div>
       <h1 className="sr-only">Fairpoint Software - Tax & Accounting Platform</h1>
       <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
